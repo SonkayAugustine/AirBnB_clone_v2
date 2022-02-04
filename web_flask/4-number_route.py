8e1ebf2a426b7f577099cbaefb2399c1a74bcdf6
SonkayAugustine <zamanisonkay@gmail.com>
2022-02-03 17:30:37 -0800

    fixed

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,56 @@
+#!/usr/bi/python3
+'''
+a script that starts a flask web application
+'''
+
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    '''
+    display Hello HBNB!
+    '''
+
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def disp_hbnb():
+    '''
+    display HBNB
+    '''
+
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    '''
+    display C followed by the value of the text
+    '''
+
+    return 'C {}'.format(text.replace('_', ' '))
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def disp_py(text='is cool'):
+   '''
+   display Python followed by default text or another
+   '''
+
+   return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<n>', strict_slashes=False)
+def disp_num(n):
+   '''
+   display n is a number only if n is an integer
+   '''
+
+   return '{:d} is a number'.format(n)
+
+
+if __name__ == '__main__':
+   app.run(host='0.0.0.0', port=5000)
